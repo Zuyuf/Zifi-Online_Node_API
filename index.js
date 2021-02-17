aa11dd3688076222f76a7d03f97ef78f919cade2
Zuyufullah Manna <zuyufmanna@gmail.com>
2021-02-17 19:12:01 +0530

    added joi-objectId, options to mongo.connect

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const categories = require("./routes/categories");
+const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
 
 const app = express();
 
+const categories = require("./routes/categories");
+const userDetails = require("./routes/userDetails");
+const users = require("./routes/users");
+
+// CONNECT to MongoDB
 mongoose
    .connect("mongodb://localhost/zifi", {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
+      useFindAndModify: false,
    })
    .then(() => console.log("Connected to MongoDB..."))
    .catch((ex) => console.error("Couldn't connect to MongoDB!!"));
 
+// PIPELINE
 app.use(express.json());
 app.use("/api/categories", categories);
+app.use("/api/userDetails", userDetails);
+app.use("/api/users", users);
 
+// Config PORT
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
