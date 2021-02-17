1c10e8bc74df9d0ca53b9e87f372bf546c10fafc
Zuyufullah Manna <zuyufmanna@gmail.com>
2021-02-17 19:18:30 +0530

    added userSchema, user model, user validation in ./models/user

---
+++ models/user.js
@@ -0,0 +1,53 @@
+const Joi = require("joi");
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+   name: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 50,
+   },
+
+   email: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 255,
+      unique: true,
+   },
+
+   password: {
+      type: String,
+      required: true,
+      minlength: 4,
+      maxlength: 1024,
+   },
+
+   isAdmin: {
+      type: Boolean,
+      default: false,
+   },
+
+   userDetails: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "UserDetail",
+   },
+});
+
+const User = mongoose.model("User", userSchema);
+
+function validateUser(user) {
+   const joiUserSchema = Joi.object({
+      name: Joi.string().min(5).max(50).required(),
+      email: Joi.string().email().min(5).max(255).required(),
+      password: Joi.string().min(4).max(255).required(),
+      isAdmin: Joi.boolean(),
+      userDetails: Joi.objectId(),
+   });
+
+   return joiUserSchema.validate(user);
+}
+
+exports.User = User;
+exports.validateUser = validateUser;
