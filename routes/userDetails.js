de56e7a327471e9f28f090d7b4d294e0a3b1c53d
Zuyufullah Manna <zuyufmanna@gmail.com>
2021-02-17 19:16:38 +0530

    added get, get:id, post, put:id, delete:id routers in ./routes/userDetails

---
+++ routes/userDetails.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const { UserDetail, validateUserDetail } = require("../models/userDetail");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+   const userDetail = await UserDetail.find();
+   res.send(userDetail);
+});
+
+router.get("/:id", async (req, res) => {
+   const userDetail = await UserDetail.findById(req.params.id);
+
+   if (!userDetail)
+      return res
+         .status(404)
+         .send("The User Details with given ID was Not Found!!");
+
+   res.send(userDetail);
+});
+
+router.post("/", async (req, res) => {
+   const { error } = validateUserDetail(req.body);
+   if (error) return res.status(400).send(error.details[0].message);
+
+   let userDetails = new UserDetail({ ...req.body });
+   userDetails = await userDetails.save();
+
+   res.send(userDetails);
+});
+
+router.put("/:id", async (req, res) => {
+   const { error } = validateUserDetail(req.body);
+   if (error) return res.status(400).send(error.details[0].message);
+
+   const userDetails = await UserDetail.findByIdAndUpdate(
+      req.params.id,
+      { ...req.body },
+      { new: true }
+   );
+
+   if (!userDetails)
+      return res
+         .status(404)
+         .send("The User Details with given ID was Not Found!!");
+
+   res.send(userDetails);
+});
+
+router.delete("/:id", async (req, res) => {
+   const userDetails = await UserDetail.findByIdAndDelete(req.params.id);
+
+   if (!userDetails)
+      return res
+         .status(404)
+         .send("The User Details with given ID was Not Found!!");
+
+   res.send(userDetails);
+});
+
+module.exports = router;
