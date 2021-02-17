719fc525d23beef0028faa147055fe5e08453eae
Zuyufullah Manna <zuyufmanna@gmail.com>
2021-02-17 13:53:18 +0530

    added put, delete, get:id In /routes/categories

--- routes/categories.js
+++ routes/categories.js
@@ -1,17 +1,53 @@
 const express = require("express");
-const { Category } = require("../models/category");
+const { Category, validateCategory } = require("../models/category");
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
    const categories = await Category.find();
-
    res.send(categories);
 });
 
+router.get("/:id", async (req, res) => {
+   const category = await Category.findById(req.params.id);
+
+   if (!category)
+      return res.status(404).send("The category with given ID was not Found!!");
+
+   res.send(category);
+});
+
 router.post("/", async (req, res) => {
+   const { error } = validateCategory(req.body);
+   if (error) return res.status(400).send(error.details[0].message);
+
    let category = new Category({ name: req.body.name });
    category = await category.save();
+
+   res.send(category);
+});
+
+router.put("/:id", async (req, res) => {
+   const { error } = validateCategory(req.body);
+   if (error) return res.status(400).send(error.details[0].message);
+
+   const category = await Category.findByIdAndUpdate(
+      req.params.id,
+      { name: req.body.name },
+      { new: true }
+   );
+
+   if (!category)
+      return res.status(404).send("The category with given ID was not Found!!");
+
+   res.send(category);
+});
+
+router.delete("/:id", async (req, res) => {
+   const category = await Category.findByIdAndDelete(req.params.id);
+
+   if (!category)
+      return res.status(404).send("The category with given ID was not Found!!");
 
    res.send(category);
 });
