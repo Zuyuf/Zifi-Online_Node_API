544843482d7f0e0eeb6da219e318cd7b156b44ae
Zuyufullah Manna <zuyufmanna@gmail.com>
2021-02-17 13:12:00 +0530

    created schema, model, validateCategory of Category

--- models/category.js
+++ models/category.js
@@ -1,2 +1,25 @@
+const { func } = require("joi");
 const Joi = require("joi");
 const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema({
+   name: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 50,
+   },
+});
+
+const Category = mongoose.model("Category", categorySchema);
+
+function validateCategory(category) {
+   const joiSchema = {
+      name: Joi.string().min(5).max(50).required(),
+   };
+
+   return Joi.validate(category, joiSchema);
+}
+
+exports.Category = Category;
+exports.validateCategory = validateCategory;
