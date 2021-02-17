d8292e79936a5231d87b032d92772ef995d92f86
Zuyufullah Manna <zuyufmanna@gmail.com>
2021-02-17 13:06:55 +0530

    added  DB string, json middleware

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
 const categories = require("./routes/categories");
 
 const app = express();
 
-app.listen(3000, () => {
-   Norm("Listening on 3000");
-});
+mongoose
+   .connect("mongodb://localhost/zifi")
+   .then(() => console.log("Connected to MongoDB..."))
+   .catch((ex) => console.error("Couldn't connect to MongoDB!!"));
+
+app.use(express.json());
+app.use("/api/categories", categories);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
