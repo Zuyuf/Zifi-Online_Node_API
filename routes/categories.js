816ecb90fd15648ac77828de6b73d1554c1bf736
Zuyufullah Manna <zuyufmanna@gmail.com>
2021-02-18 18:07:06 +0530

    added auth, admin protection to /api/categories Routes

--- routes/categories.js
+++ routes/categories.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const { Category, validateCategory } = require("../models/category");
+const auth = require("../middlewares/auth");
+const admin = require("../middlewares/admin");
 
 const router = express.Router();
 
@@ -17,7 +19,7 @@ router.get("/:id", async (req, res) => {
    res.send(category);
 });
 
-router.post("/", async (req, res) => {
+router.post("/", [auth, admin], async (req, res) => {
    const { error } = validateCategory(req.body);
    if (error) return res.status(400).send(error.details[0].message);
 
@@ -27,7 +29,7 @@ router.post("/", async (req, res) => {
    res.send(category);
 });
 
-router.put("/:id", async (req, res) => {
+router.put("/:id", [auth, admin], async (req, res) => {
    const { error } = validateCategory(req.body);
    if (error) return res.status(400).send(error.details[0].message);
 
@@ -43,7 +45,7 @@ router.put("/:id", async (req, res) => {
    res.send(category);
 });
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", [auth, admin], async (req, res) => {
    const category = await Category.findByIdAndDelete(req.params.id);
 
    if (!category)
