4a0e44fa11278aa42102a9d9fc3eca00ddd09608
Zuyufullah Manna <zuyufmanna@gmail.com>
2021-02-19 18:38:06 +0530

    created validateObjectId middlewre

--- middlewares/validateObjectId.js
+++ middlewares/validateObjectId.js
@@ -1,8 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-module.exports = function(req, res, next) {
-    if (!mongoose.Types.ObjectId.isValid(req.params.id))
-        return res.status(404).send('Invalid ID')
-    
-    next()
-}
+module.exports = function (req, res, next) {
+   if (!mongoose.Types.ObjectId.isValid(req.params.id))
+      return res.status(404).send("Invalid ID");
+   next();
+};
