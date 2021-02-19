3db14148dfeb019b1f302441a0bafa86ba2465c0
Zuyufullah Manna <zuyufmanna@gmail.com>
2021-02-19 18:42:01 +0530

    added validate middleware in ./routes/userDetails

--- routes/userDetails.js
+++ routes/userDetails.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const _ = require("lodash");
+
 const { UserDetail, validateUserDetail } = require("../models/userDetail");
+
 const auth = require("../middlewares/auth");
+const validate = require("../middlewares/validate");
 const validateObjectId = require("../middlewares/validateObjectId");
 
 const router = express.Router();
+
+//
 
 router.get("/", [auth], async (req, res) => {
    const userDetail = await UserDetail.find();
    res.send(userDetail);
 });
 
+//
 router.get("/:id", [auth, validateObjectId], async (req, res) => {
    const userDetail = await UserDetail.findById(req.params.id);
 
@@ -22,34 +28,35 @@ router.get("/:id", [auth, validateObjectId], async (req, res) => {
    res.send(userDetail);
 });
 
-router.post("/", [auth], async (req, res) => {
-   const { error } = validateUserDetail(req.body);
-   if (error) return res.status(400).send(error.details[0].message);
-
+//
+router.post("/", [auth, validate(validateUserDetail)], async (req, res) => {
    let userDetails = new UserDetail(UserDetail.pickReqiuredParams(req.body));
    userDetails = await userDetails.save();
 
    res.send(userDetails);
 });
 
-router.put("/:id", [auth, validateObjectId], async (req, res) => {
-   const { error } = validateUserDetail(req.body);
-   if (error) return res.status(400).send(error.details[0].message);
+//
+router.put(
+   "/:id",
+   [auth, validateObjectId, validate(validateUserDetail)],
+   async (req, res) => {
+      const userDetails = await UserDetail.findByIdAndUpdate(
+         req.params.id,
+         UserDetail.pickReqiuredParams(req.body),
+         { new: true }
+      );
 
-   const userDetails = await UserDetail.findByIdAndUpdate(
-      req.params.id,
-      UserDetail.pickReqiuredParams(req.body),
-      { new: true }
-   );
+      if (!userDetails)
+         return res
+            .status(404)
+            .send("The User Details with given ID was Not Found!!");
 
-   if (!userDetails)
-      return res
-         .status(404)
-         .send("The User Details with given ID was Not Found!!");
+      res.send(userDetails);
+   }
+);
 
-   res.send(userDetails);
-});
-
+//
 router.delete("/:id", [auth, validateObjectId], async (req, res) => {
    const userDetails = await UserDetail.findByIdAndDelete(req.params.id);
 
