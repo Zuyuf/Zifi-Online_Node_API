dde9186ded204e612874d031b3cf458bf7823073
Zuyufullah Manna <zuyufmanna@gmail.com>
2021-02-19 18:43:20 +0530

    updated db string to be dynamically fetched from config based on NODE_ENV

--- startup/db.js
+++ startup/db.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
+const config = require("config");
 
 module.exports = function () {
    // CONNECT to MongoDB
+   const db_string = config.get("db");
+
    mongoose
-      .connect("mongodb://localhost/zifi", {
+      .connect(db_string, {
          useNewUrlParser: true,
          useFindAndModify: false,
       })
-      .then(() => console.log("Connected to MongoDB..."));
+      .then(() => console.log(`Connected to MongoDB :  ${db_string}  `));
    // .catch((ex) => console.error("Couldn't connect to MongoDB!!"));
 };
