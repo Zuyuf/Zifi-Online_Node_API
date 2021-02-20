6d2b0973d1a80886035130935e1512435b304ac1
Zuyufullah Manna <zuyufmanna@gmail.com>
2021-02-20 14:32:08 +0530

    added option useUnifiedTopology

--- startup/db.js
+++ startup/db.js
@@ -9,6 +9,7 @@ module.exports = function () {
       .connect(db_string, {
          useNewUrlParser: true,
          useFindAndModify: false,
+         useUnifiedTopology: true,
       })
       .then(() => console.log(`Connected to MongoDB :  ${db_string}  `));
    // .catch((ex) => console.error("Couldn't connect to MongoDB!!"));
