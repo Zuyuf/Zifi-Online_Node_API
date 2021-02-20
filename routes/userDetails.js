941d95fed87f0ae4a4d3fd55f5b7b35b2682a628
Zuyufullah Manna <zuyufmanna@gmail.com>
2021-02-20 19:33:18 +0530

    updated GET / (admin middleware), GET /:id to /me and also updated POST /, PUT /:id logics

--- routes/userDetails.js
+++ routes/userDetails.js
@@ -4,34 +4,39 @@ const _ = require("lodash");
 const { UserDetail, validateUserDetail } = require("../models/userDetail");
 
 const auth = require("../middlewares/auth");
+const admin = require("../middlewares/admin");
 const validate = require("../middlewares/validate");
 const validateObjectId = require("../middlewares/validateObjectId");
+const { cond } = require("lodash");
 
 const router = express.Router();
 
 //
 
-router.get("/", [auth], async (req, res) => {
+router.get("/", [auth, admin], async (req, res) => {
    const userDetail = await UserDetail.find();
+
    res.send(userDetail);
 });
 
 //
-router.get("/:id", [auth, validateObjectId], async (req, res) => {
-   const userDetail = await UserDetail.findById(req.params.id);
+router.get("/me", [auth], async (req, res) => {
+   const userDetail = await UserDetail.find({ user_id: req.user._id });
 
-   if (!userDetail)
+   if (!userDetail.length)
       return res
          .status(404)
-         .send("The User Details with given ID was Not Found!!");
+         .send("The User Details with given User ID was Not Found!!");
 
    res.send(userDetail);
 });
 
 //
 router.post("/", [auth, validate(validateUserDetail)], async (req, res) => {
+   req.body.user_id = req.user._id;
+
    let userDetails = new UserDetail(UserDetail.pickReqiuredParams(req.body));
-   userDetails = await userDetails.save();
+   await userDetails.save();
 
    res.send(userDetails);
 });
@@ -41,6 +46,8 @@ router.put(
    "/:id",
    [auth, validateObjectId, validate(validateUserDetail)],
    async (req, res) => {
+      req.body.user_id = req.user._id;
+
       const userDetails = await UserDetail.findByIdAndUpdate(
          req.params.id,
          UserDetail.pickReqiuredParams(req.body),
