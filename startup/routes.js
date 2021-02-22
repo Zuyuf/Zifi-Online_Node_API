12341b6a0cd7e5c4cb59719c41840f5f75d92a8e
Zuyufullah Manna <zuyufmanna@gmail.com>
2021-02-22 11:21:41 +0530

    added /api/userDetails/addresses and /cards routes in Pipeline

--- startup/routes.js
+++ startup/routes.js
@@ -3,6 +3,8 @@ const express = require("express");
 // Routes
 const categories = require("../routes/categories");
 const userDetails = require("../routes/userDetails");
+const userDetailsAddresses = require("../routes/userDetails/addresses");
+const userDetailsCards = require("../routes/userDetails/cards");
 const users = require("../routes/users");
 const auth = require("../routes/auth");
 const error = require("../middlewares/error");
@@ -12,6 +14,8 @@ module.exports = function (app) {
    app.use(express.json());
    app.use("/api/categories", categories);
    app.use("/api/userDetails", userDetails);
+   app.use("/api/userDetails/addresses", userDetailsAddresses);
+   app.use("/api/userDetails/cards", userDetailsCards);
    app.use("/api/users", users);
    app.use("/api/auth", auth);
    app.use(error);
