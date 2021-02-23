d9c5af29e1e37bec2b5ca3c959435e24f7eb28b8
Zuyufullah Manna <zuyufmanna@gmail.com>
2021-02-23 12:44:45 +0530

    Created new /api/userDetails/cards routes GET/me, GET/:id, POST /, PUT /:id, DELETE /:id

--- routes/userDetails/cards.js
+++ routes/userDetails/cards.js
@@ -1,17 +1,83 @@
 const express = require("express");
 const _ = require("lodash");
 
-const { UserDetail, validateAddress } = require("../../models/userDetail");
-const { User } = require("../../models/user");
+const {
+   UserDetail,
+   validateCard,
+   updateCard,
+   deleteCard,
+   validatIfUserExits: vUserE,
+   validateIfUserDetailsExits: vUsrDetlsE,
+   validateIfCardIdExits: vCardE,
+} = require("../../models/userDetail");
 
 const auth = require("../../middlewares/auth");
-const admin = require("../../middlewares/admin");
-const validate = require("../../middlewares/validate");
-const validateObjectId = require("../../middlewares/validateObjectId");
+const val = require("../../middlewares/validate");
+const valW = require("../../middlewares/validateWrapper");
+const valObjId = require("../../middlewares/validateObjectId");
 
 const router = express.Router();
 
 //
+//
+
+router.get("/me", [auth, valW(vUsrDetlsE)], async (req, res) => {
+   res.send(req.userDetails.cards);
+});
+
+//
+router.get("/:id", [auth, valObjId, valW(vCardE)], async (req, res) => {
+   const card = _.find(req.userDetails.cards, "_id", req.params.id);
+
+   res.send(card);
+});
+
+//
+router.post(
+   "/",
+   [auth, val(validateCard), valW(vUserE), valW(vUsrDetlsE)],
+   async (req, res) => {
+      const cards_data = UserDetail.pickCardParams(req.body);
+
+      const updatedDoc = await UserDetail.findByIdAndUpdate(
+         req.userDetails._id,
+         { $push: { cards: cards_data } },
+         { new: true }
+      );
+
+      res.send(updatedDoc.cards);
+   }
+);
+
+//
+router.put(
+   "/:id",
+   [
+      auth,
+      valObjId,
+      val(validateCard),
+      valW(vUserE),
+      valW(vUsrDetlsE),
+      valW(vCardE),
+   ],
+   async (req, res) => {
+      const cards_data = UserDetail.pickCardParams(req.body);
+
+      const { updatedDoc, updatedCardIndex } = await updateCard(
+         cards_data,
+         req.params.id
+      );
+
+      res.send(updatedDoc.cards[updatedCardIndex]);
+   }
+);
+
+//
+router.delete("/:id", [auth, valObjId, valW(vCardE)], async (req, res) => {
+   const { updatedDoc, deletedCard } = await deleteCard(req.params.id);
+
+   res.send(deletedCard);
+});
 
 //
 module.exports = router;
