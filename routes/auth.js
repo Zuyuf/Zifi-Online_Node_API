d277011f11034d1c9d51c706b929b8a93ac46576
Zuyufullah Manna <zuyufmanna@gmail.com>
2021-02-23 12:39:10 +0530

    UPDATED - error message is sent in json object

--- routes/auth.js
+++ routes/auth.js
@@ -11,10 +11,12 @@ const router = express.Router();
 
 router.post("/", validate(validateUserLogin), async (req, res) => {
    let user = await User.findOne({ email: req.body.email });
-   if (!user) return res.status(400).send("Invalid Email or Password");
+   if (!user)
+      return res.status(400).send({ message: "Invalid Email or Password" });
 
    const validPassword = await bcrypt.compare(req.body.password, user.password);
-   if (!validPassword) return res.status(400).send("Invalid Email or Password");
+   if (!validPassword)
+      return res.status(400).send({ message: "Invalid Email or Password" });
 
    const token = user.generateAuthToken();
    res.send({ token });
